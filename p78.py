526490181b629f67837e0fa45df8560f750af159
Anna Komha <ana.ko.2012@gmail.com>
2021-04-23 14:47:43 +0300

    3 random spirals

---
+++ p78.py
@@ -0,0 +1,38 @@
+
+# import
+import turtle
+import random
+
+turtle.colormode(255)
+turtle.bgcolor(0,0,0)   # background color - black
+
+def spirala(t):
+    """
+	Draws a spiral with a given tartle object (t)
+	Starting point is random 
+    Color is random blue
+    Width is random
+    """
+    t.penup()
+    t.setx(random.randrange(-200,200))
+    t.sety(random.randrange(-200,200))
+    t.pencolor(random.randrange(0,255),random.randrange(0,255),200)
+    t.width(random.randrange(2,9))
+    t.pendown()
+
+    for i in range(120):
+    	t.forward(20+i)
+    	t.left(30 - i/1.5)
+# -- end of spirala(t)
+
+###   Main      ###
+# init turtle
+t1 = turtle.Turtle()
+t1.speed(0)
+t1.shape('circle')
+
+#Drawing 3 spirals from random position
+for z in range(3):
+    spirala(t1)
+
+turtle.done()
