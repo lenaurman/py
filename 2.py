7e53ca153a5e6b170ce8ad2f12be3f968848a2bc
Anna Komha <ana.ko.2012@gmail.com>
2021-04-27 22:32:57 +0300

    onclick

--- 2.py
+++ 2.py
@@ -27,6 +27,9 @@ def spirala(t):
     	t.left(30 - i/1.5)
 # -- end of spirala(t)
 
+def turn(x,y):
+    t2.left(90)
+    t2.forward(77)
 
 #####################
   ###   Main      ###
@@ -37,12 +40,22 @@ t1 = turtle.Turtle()
 t1.speed(0)
 t1.shape('circle')
 
+t2 = turtle.Turtle()
+t2.shape('turtle')
+t2.pencolor('red')
+t2.forward(100)
+t2.onclick(turn)
+t2.forward(200)
+
+
+#t1.onclick(spirala)
+
 #Drawing 3 spirals with random position, color and width
 for z in range(3):
     spirala(t1)
 
 # https://pythonprogramminglanguage.com/user-input-python/
-name = input("Enter a name: ")
-print(name)    
+#name = input("Enter a name: ")
+#print(name)    
 
 turtle.done()
