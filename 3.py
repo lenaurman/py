8c39fc75fe55586827f8837b3b8fdde42bdb91af
Anna Komha <ana.ko.2012@gmail.com>
2021-04-29 17:13:00 +0300

    Draws random spirals on click and fill them

--- 3.py
+++ 3.py
@@ -1,4 +1,4 @@
-# import package
+# import 
 import turtle
 import random
 
@@ -14,16 +14,23 @@ def spiralaa():
     """ Draws a spiral on a tartle Screen with random Starting point, Color and Width 
     """
                 # Random color
-    turtle.pencolor(random.randrange(0,111),random.randrange(0,77),200)
+    turtle.penup()            
+    turtle.pencolor(random.randrange(0,33),random.randrange(0,77),random.randrange(150,200))
                 # Random width
     turtle.width(random.randrange(2,13))
                 # Random direction
-    turtle.setheading(199)
-    #turtle.setheading(random.randrange(1,70))
+    turtle.setheading(random.randrange(1,360))
+    
+    #turtle.color('red', 'yellow')
+    turtle.begin_fill()
+    print('Color: ', turtle.pencolor(), ' Heading: ', turtle.heading())
+    
     turtle.pendown()
-    for i in range(19):
-    	turtle.forward(20+i)
-    	turtle.left(30 - i/1.5)
+    for i in range(33):
+        turtle.forward(20+i)
+        turtle.left(30 - i/1.5)
+    turtle.end_fill()
+    #turtle.done()    
 
 ### Main code #######################################
 #####################################################
@@ -32,7 +39,7 @@ def spiralaa():
 turtle.colormode(255) 
 turtle.bgcolor(0,0,0)   # background color - black
 turtle.pencolor(255, 200, 175)
-turtle.speed(1) # turtle speed to slowest
+turtle.speed(0) # turtle speed to slowest
 
 # draw a spiral
 spiralaa()
