e9c7b794efbe323be2ac5f77b1cae198c33a833e
Anna Komha <ana.ko.2012@gmail.com>
2021-05-04 23:04:41 +0300

    it doesn't listen to me!

--- step_3_to_magic.py
+++ step_3_to_magic.py
@@ -1,37 +1,9 @@
+### import 
 import random
 from turtle import *
 
-
-### import turtle
-
-#wn = turtle.Screen()
-#wn.title('Chudesnye spirali')
-#wn.bgcolor('black')
-#wn.setup(width=800, height=600)
-#wn.tracer()
-
-#spirala1 = turtle.Turtle()
-#spirala2 = turtle.Turtle()
-#spirala3 = turtle.Turtle()
-
-#spirala1.setposition(100,0)
-#spirala2.setposition(0,0)
-#spirala3.setposition(-100,0)
-
-#spirala1.pencolor('red')
-#spirala2.pencolor('green')
-#spirala3.pencolor('blue')
-
-#spirala1.forward(20)
-#spirala2.forward(50)
-#spirala3.forward(70)
-
-#spirala1.onclick
-#####################################
-
-
+### Class  Spirartle inherits Turtle
 class Spirartle(Turtle):
-
 
     def __init__(self):
         """ Create a new Turtle as Spirala and rundomize it's 
@@ -47,9 +19,8 @@ class Spirartle(Turtle):
         bbb = random.randrange(110,255)
         color = (rrr, ggg, bbb)
         self.color(color) # set color        
-        """ set Width """
-        widthSpi = random.randrange(2,12)         
-        self.width(widthSpi)
+        """ set Width """        
+        self.width(random.randrange(2,12))
         """ be ready at your position"""
         xx = random.randrange(-1 * int(screen.canvwidth/2), int(screen.canvwidth)/2)
         yy = random.randrange(-1 * int(screen.canvheight/2), int(screen.canvheight)/2)
@@ -61,11 +32,11 @@ class Spirartle(Turtle):
         """
         for i in range(33):
             self.forward(20+1)
-            self.left(30 - i/1.6)                
+            self.left(30 - i/1.6)                           
 
     def onclick(self):
         self.color = (200,0,0)
-        self.setposition = self.position - 70
+       # self.setposition = self.position - 70
         self.drawMe()
 
     def myColor(self):
@@ -84,17 +55,25 @@ screen.title ('Random color spirals')
 #startx = -150
 #starty = 150 
 
+screen.listen()
 
-soii1 = Spirartle()
-soii2 = Spirartle()
-soii3 = Spirartle()
-soii4 = Spirartle()
-soii5 = Spirartle()
-soii6 = Spirartle()
-soii7 = Spirartle()
+s1 = Spirartle()
+s2 = Spirartle()
+s3 = Spirartle()
+s4 = Spirartle()
+#s5 = Spirartle()
+#s6 = Spirartle()
+#s7 = Spirartle()
 
-soii1.drawMe()
-soii2.drawMe()
-soii3.drawMe()
 
-done()
+s1.drawMe()
+s2.drawMe()
+
+screen.listen()
+screen.mainloop()
+
+screen.onclick(s1.drawMe())
+
+screen.onclick(s3.drawMe())
+
+#done()
