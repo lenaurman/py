d8103cec6522960757c1f7ccce15f4f51c63df61
Anna Komha <ana.ko.2012@gmail.com>
2021-05-05 23:42:55 +0300

    Draws a spiral and other stuff

--- step_3_to_magic.py
+++ step_3_to_magic.py
@@ -14,13 +14,16 @@ class Spirartle(Turtle):
         Turtle.__init__(self,shape='circle')
         self.penup()
         """ set Color """
-        rrr = random.randrange(0,255)
+        rrr = random.randrange(0,77)
         ggg = random.randrange(0,77)
         bbb = random.randrange(110,255)
         color = (rrr, ggg, bbb)
         self.color(color) # set color        
         """ set Width """        
-        self.width(random.randrange(2,12))
+        self.width(random.randrange(1,12))
+        """ set heading """        
+        self.setheading(random.randrange(1,360))
+
         """ be ready at your position"""
         xx = random.randrange(-1 * int(screen.canvwidth/2), int(screen.canvwidth)/2)
         yy = random.randrange(-1 * int(screen.canvheight/2), int(screen.canvheight)/2)
@@ -31,16 +34,15 @@ class Spirartle(Turtle):
         """
         """
         for i in range(33):
-            self.forward(20+1)
+            self.forward(20+i)
             self.left(30 - i/1.6)                           
-
-    def onclick(self):
-        self.color = (200,0,0)
-       # self.setposition = self.position - 70
-        self.drawMe()
-
+        
     def myColor(self):
         return self.color       
+
+
+def h1():
+    print('pressseddd')
 
 #####################################
 ######################## Main 
@@ -55,25 +57,32 @@ screen.title ('Random color spirals')
 #startx = -150
 #starty = 150 
 
-screen.listen()
-
 s1 = Spirartle()
 s2 = Spirartle()
 s3 = Spirartle()
 s4 = Spirartle()
-#s5 = Spirartle()
+s5 = Spirartle()
 #s6 = Spirartle()
 #s7 = Spirartle()
 
+#s1.drawMe()
+#s2.drawMe()
+s3.drawMe()
+s4.drawMe()
+s5.drawMe()
 
-s1.drawMe()
-s2.drawMe()
 
+#screen.onclick(s3.drawMe())
+#ttt = screen.turtles()
+#ttt[1].reset()
+
+
+#screen.onclick(s1.drawMe())
+#screen.onclick(s2.drawMe())
+
+screen.onkeypress(s3.reset(), "Up")
+screen.onkeypress(s4.reset(), "Down")
+
+screen.onscreenclick(goto(0,0))
 screen.listen()
 screen.mainloop()
-
-screen.onclick(s1.drawMe())
-
-screen.onclick(s3.drawMe())
-
-#done()
