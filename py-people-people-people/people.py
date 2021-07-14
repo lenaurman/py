8e26e3f10614d75f35e6890ec846b3351f674790
annakoopenu <ana.ko.2012@gmail.com>
2021-07-15 01:28:07 +0300

    added years col

--- py-people-people-people/people.py
+++ py-people-people-people/people.py
@@ -3,25 +3,37 @@ import pandas as pd
 p1 = {
     'name' : 'Haruki Murakami',
     'titles' : ['writer'],
-    'creations' : ['Man in the Taxi']
+    'creations' : ['Man in the Taxi','The Wind-Up Bird Chronicle'],
+    'creations_year' : [2000,1998]  #need a check
 }
 
 p2 = {
     'name' : 'David Lynch',
     'titles' : ['filmmaker'],
-    'creations' : ['Lost Highway']
+    'creations' : ['Lost Highway'],
+    'creations_year' : [1997] #need a check
 }
 
 p3 = {
     'name' : 'Michelle Gurevich',
     'titles' : ['singer'],
-    'creations' : ['Party Girl']
+    'creations' : ['Party Girl'],
+    'creations_year' : [2010] #need a check
 }
+
+#
 ## add p4 please
-pp = [p1, p2, p3]
-ppd = pd.DataFrame(pp)
+#
+# DataFrame from list pf dicts
+ppd = pd.DataFrame([p1, p2, p3])
 
-
-ppd.to_csv('people_you_must_know.csv')
+print()
+print('---------- People you must know dataset ----------------')
+print()
 print(ppd)
+ppd.to_csv('people_you_must_know.csv')
+print('Saved to csv.. i hope..')
+print()
+
 print("Add p4 please")
+print()
